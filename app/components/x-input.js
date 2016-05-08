52678f60f23564eeebbbe72095a31d835d3a3587
David Salgado <david@digitalronin.com>
2016-05-08 12:10:32 +0100

    bugfix: Ember.computed, not computed

--- app/components/x-input.js
+++ app/components/x-input.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['input-field'],
   type: 'text',
-  _errorMessages: computed('errors.[]', function() {
-    return (this.get('errors') || []).join(', ')
+  _errorMessages: Ember.computed('errors.[]', function() {
+    return (this.get('errors') || []).join(', ');
   })
 });
